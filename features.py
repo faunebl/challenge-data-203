d1554f080705c0cd7a9ccb9e2615b3859f610fc6
faunebl <faunebl@protonmail.com>
2025-03-16 20:46:10 +0100

    fixed issue with stacking feature interactions -> works

--- features.py
+++ features.py
@@ -25,15 +25,15 @@ class FeaturesFrame(pl.DataFrame):
         )
 
     def add_feature_interactions(self):
-        return self.with_columns(
+        return type(self)(self.with_columns(
             [
                 (pl.col(a) * pl.col(b)).alias(f"{a}_{b}_multiplied")
                 for a, b in itertools.combinations(self.select(cs.numeric()).columns, 2)
             ]
-        )
+        ))
 
     def add_feature_log(self):
-        return self.with_columns(
+        return type(self)(self.with_columns(
             [
                 pl.when(pl.col(c) > 0)
                 .then(pl.col(c).log())
@@ -41,17 +41,17 @@ class FeaturesFrame(pl.DataFrame):
                 .alias(f"{c}_log")
                 for c in self.select(cs.numeric()).columns
             ]
-        )
+        ))
 
     def add_feature_square(self):
-        return self.with_columns(
+        return type(self)(self.with_columns(
             [pl.col(c).pow(2).alias(f"{c}_square") for c in self.select(cs.numeric()).columns]
-        )
+        ))
     
     def add_feature_sqrt(self):
-        return self.with_columns(
+        return type(self)(self.with_columns(
             [pl.col(c).sqrt().alias(f"{c}_square") for c in self.select(cs.numeric()).columns]
-        )
+        ))
 
     # @classmethod
     # def optimize_categorical(self):
@@ -65,4 +65,4 @@ class FeaturesFrame(pl.DataFrame):
     def encode_one_hot(self):
         for col in self.select(cs.string()).columns:
             self = self.with_columns(pl.select(col).to_dummies(prefix=col)).drop(col)
-        return self
+        return type(self)(self)
